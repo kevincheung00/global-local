09f40da48c99b66f288c1bf2187089f97ab92dbc
kevincheung00 <kevin.cheung.sh@gmail.com>
2020-02-01 14:04:37 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
-  const APP_ID = "a36541af";
-  const APP_KEY = "b8929d43d1f37d1d8545a937c4ec6a36";
 
   return(
     <div className="App">
-      <h1>Hello React</h1>
+      <h1>Global Local</h1>
+
     </div>
   )
 }
