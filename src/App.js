d93b2483bf875568f7d89b71b6b8a991befd5c1e
kevincheung00 <kevin.cheung.sh@gmail.com>
2020-02-01 13:29:45 -0500

    ye

--- src/App.js
+++ src/App.js
@@ -2,25 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+  const APP_ID = "a36541af";
+  const APP_KEY = "b8929d43d1f37d1d8545a937c4ec6a36";
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> fland.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello React</h1>
     </div>
-  );
+  )
 }
 
 export default App;
