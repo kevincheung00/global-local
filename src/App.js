bd0cf915b9f8d44d602265cf9c3fdce6a16dda59
kevincheung00 <kevin.cheung.sh@gmail.com>
2020-02-01 17:51:03 -0500

    yay

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Component } from "react"
 import { Router, Route, Link, IndexRoute, browserHistory } from 'react-router'
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import { hashRouter } from "react-router-dom"
 import Map from './components/map'
 import FirstComponent from "./components/firstComponent"
@@ -30,33 +30,38 @@ const tourguide_data = [
     name: "Alvina Lin",
     age: 19,
     rating: 5
+  },
+
+  {
+    name: "Fland Pan",
+    age: 19,
+    rating: 5
+  },
+  
+  {
+    name: "Leanne Li",
+    age: 19,
+    rating: 5
   }
 
 ]
 
 class App extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
+    // constructor(props) {
+    //   super(props);
+    //   this.state = {
     
-    }
-}
+    //   }
+    // }
   
   
   render() {
       return(
         <div className="App">
-<<<<<<< HEAD
           <h1>Global Local</h1>
-          
-          
           <About />
           <Map />
 
-=======
-          <Navibar />
-          <Map />
->>>>>>> b9fa75fa534227a77710e9c594f1665016c294fc
         </div>
       )
   }}
